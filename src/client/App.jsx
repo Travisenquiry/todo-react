1207b44ab2793ed9de11317832f5611663ecea34
travisenquiry <travisenquiry@gmail.com>
2020-06-01 17:12:01 +0800

    completed mvp

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      list: []
+    }
+  }
+
+  executeMe(){
+    let input = document.getElementById("input-list").value;
+    let updatedList = [...this.state.list];
+    updatedList.push(input);
+    this.setState({list: updatedList});
+  }
+
   render() {
+    let list = this.state.list.map(item => {
+      return <li>{item}</li>
+    });
     return (
       <div>
-        Welcome.
+        <input id="input-list"></input>
+        <button onClick={() => {this.executeMe();}}>press me</button>
+        <div>
+          <h3>My Todo List</h3>
+          <ul>
+            {list}
+          </ul>
+        </div>
       </div>
     );
   }
